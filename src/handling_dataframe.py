c7a762fb53bd0e3d1c2849eb59d5f641bc73ca7a
nhut-ntm <nhutntmuel@gmail.com>
2023-10-21 08:52:51 +0700

    Modified. Add new functions to produce pairs formation summary table

--- src/handling_dataframe.py
+++ src/handling_dataframe.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import itertools
+from itertools import combinations
 
 
 def reformat_dataframe(
@@ -82,3 +84,93 @@ def resample_dataframe(
     resampled_df = df.set_index(date_column).resample(space_freq).asfreq()
     
     return resampled_df
+
+
+def pairs_formation_result_summary(
+    gatev_pairs_list: list,
+    eg_pairs_list: list,
+    eg_pairs_swap_list: list,
+    johansen_pairs_list: list
+) -> pd.DataFrame:
+    
+    # Combine all four lists into a single list
+    all_pairs = gatev_pairs_list + eg_pairs_list + eg_pairs_swap_list + johansen_pairs_list
+
+    # Use a set to store unique values
+    unique_values = set()
+
+    # Iterate through the lists and add their elements to the set
+    for pair in all_pairs:
+        unique_values.update(pair)
+
+    # Convert the set back to a list
+    unique_values_list = list(unique_values)
+
+    # Sort the list if needed
+    unique_values_list.sort()
+    
+    # Create a list to store the combinations
+    combinations_list = []
+
+    # Use itertools.combinations to generate the combinations
+    for combo in combinations(unique_values_list, 2):
+        combinations_list.append(combo)
+    
+    # Create an empty DataFrame with the specified columns
+    columns = ['Stock A', 'Stock B', 'Gatev Pairs', 'EG Pairs', 'EG Pairs Swap', 'Johansen Pairs']
+    df = pd.DataFrame(columns=columns)
+    
+    # Create an empty list of dictionaries
+    data = []
+
+    # Iterate through combinations and check if they appear in the given lists
+    for combo in combinations_list:
+        stock_a, stock_b = combo
+        row = {'Stock A': stock_a, 'Stock B': stock_b}
+
+        count_x = 0  # Initialize the count of 'x' marks for this combination
+
+        for pair in gatev_pairs_list:
+            if [stock_a, stock_b] == pair or [stock_b, stock_a] == pair:
+                row['Gatev Pairs'] = 'x'
+                count_x += 1
+
+        for pair in eg_pairs_list:
+            if [stock_a, stock_b] == pair or [stock_b, stock_a] == pair:
+                row['EG Pairs'] = 'x'
+                count_x += 1
+
+        for pair in eg_pairs_swap_list:
+            if [stock_a, stock_b] == pair or [stock_b, stock_a] == pair:
+                row['EG Pairs Swap'] = 'x'
+                count_x += 1
+
+        for pair in johansen_pairs_list:
+            if [stock_a, stock_b] == pair or [stock_b, stock_a] == pair:
+                row['Johansen Pairs'] = 'x'
+                count_x += 1
+
+        row['Count'] = count_x  # Add the count of 'x' marks for this combination
+        data.append(row)
+
+    # Create the DataFrame from the list of dictionaries
+    df = pd.DataFrame(data)
+
+    # Create an additional row at the end that counts the number of 'x' marks
+    count_row = {'Stock A': 'Count', 'Stock B': ''}
+    for col in df.columns[2:]:
+        count_row[col] = (df[col] == 'x').sum()
+
+    df = df.append(count_row, ignore_index=True)
+
+    # Reorder the columns to have 'Count' as the last column
+    df = df[['Stock A', 'Stock B', 'Gatev Pairs', 'EG Pairs', 'EG Pairs Swap', 'Johansen Pairs', 'Count']]
+
+    # Fill NaN values with an empty string for better representation
+    df = df.fillna('')   
+    
+    # Select pairs that satisfy more than one of the tests
+    df_pairs = df[df['Count']>0]
+    df_pairs = df_pairs.reset_index(drop=True)
+    
+    return df_pairs
